a10485bee3e23aef9587805e5e4cde568f4459de
Ramtinboreili <ramtin.bor7hp@gmail.com>
2024-09-15 23:01:51 +0330

    start develop

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,131 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class $AssetsPngGen {
+  const $AssetsPngGen();
+
+  /// File path: assets/png/avatar.png.html
+  String get avatarPng => 'assets/png/avatar.png.html';
+
+  /// File path: assets/png/main_logo.png.html
+  String get mainLogoPng => 'assets/png/main_logo.png.html';
+
+  /// File path: assets/png/unnamed.png.html
+  String get unnamedPng => 'assets/png/unnamed.png.html';
+
+  /// List of all assets
+  List<String> get values => [avatarPng, mainLogoPng, unnamedPng];
+}
+
+class $AssetsSvgGen {
+  const $AssetsSvgGen();
+
+  /// File path: assets/svg/avatar.svg.html
+  String get avatarSvg => 'assets/svg/avatar.svg.html';
+
+  /// File path: assets/svg/back.svg.html
+  String get backSvg => 'assets/svg/back.svg.html';
+
+  /// File path: assets/svg/cancelled.svg.html
+  String get cancelledSvg => 'assets/svg/cancelled.svg.html';
+
+  /// File path: assets/svg/cart.svg.html
+  String get cartSvg => 'assets/svg/cart.svg.html';
+
+  /// File path: assets/svg/clasic.svg.html
+  String get clasicSvg => 'assets/svg/clasic.svg.html';
+
+  /// File path: assets/svg/close.svg.html
+  String get closeSvg => 'assets/svg/close.svg.html';
+
+  /// File path: assets/svg/delete.svg.html
+  String get deleteSvg => 'assets/svg/delete.svg.html';
+
+  /// File path: assets/svg/delivered.svg.html
+  String get deliveredSvg => 'assets/svg/delivered.svg.html';
+
+  /// File path: assets/svg/desktop.svg.html
+  String get desktopSvg => 'assets/svg/desktop.svg.html';
+
+  /// File path: assets/svg/digital.svg.html
+  String get digitalSvg => 'assets/svg/digital.svg.html';
+
+  /// File path: assets/svg/home.svg.html
+  String get homeSvg => 'assets/svg/home.svg.html';
+
+  /// File path: assets/svg/in_proccess.svg.html
+  String get inProccessSvg => 'assets/svg/in_proccess.svg.html';
+
+  /// File path: assets/svg/left_arrow.svg.html
+  String get leftArrowSvg => 'assets/svg/left_arrow.svg.html';
+
+  /// File path: assets/svg/location.svg.html
+  String get locationSvg => 'assets/svg/location.svg.html';
+
+  /// File path: assets/svg/minus.svg.html
+  String get minusSvg => 'assets/svg/minus.svg.html';
+
+  /// File path: assets/svg/phone.svg.html
+  String get phoneSvg => 'assets/svg/phone.svg.html';
+
+  /// File path: assets/svg/plus.svg.html
+  String get plusSvg => 'assets/svg/plus.svg.html';
+
+  /// File path: assets/svg/postal_code.svg.html
+  String get postalCodeSvg => 'assets/svg/postal_code.svg.html';
+
+  /// File path: assets/svg/search.svg.html
+  String get searchSvg => 'assets/svg/search.svg.html';
+
+  /// File path: assets/svg/smart.svg.html
+  String get smartSvg => 'assets/svg/smart.svg.html';
+
+  /// File path: assets/svg/sort.svg.html
+  String get sortSvg => 'assets/svg/sort.svg.html';
+
+  /// File path: assets/svg/user.svg.html
+  String get userSvg => 'assets/svg/user.svg.html';
+
+  /// File path: assets/svg/user_menu.svg.html
+  String get userMenuSvg => 'assets/svg/user_menu.svg.html';
+
+  /// List of all assets
+  List<String> get values => [
+        avatarSvg,
+        backSvg,
+        cancelledSvg,
+        cartSvg,
+        clasicSvg,
+        closeSvg,
+        deleteSvg,
+        deliveredSvg,
+        desktopSvg,
+        digitalSvg,
+        homeSvg,
+        inProccessSvg,
+        leftArrowSvg,
+        locationSvg,
+        minusSvg,
+        phoneSvg,
+        plusSvg,
+        postalCodeSvg,
+        searchSvg,
+        smartSvg,
+        sortSvg,
+        userSvg,
+        userMenuSvg
+      ];
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsPngGen png = $AssetsPngGen();
+  static const $AssetsSvgGen svg = $AssetsSvgGen();
+}
