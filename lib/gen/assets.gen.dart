465dc38e9d4dcc70e7adbfcc518b891cfb5af2e6
Ramtinboreili <ramtin.bor7hp@gmail.com>
2024-09-16 11:17:59 +0330

    init assets with fluttergen

--- lib/gen/assets.gen.dart
+++ lib/gen/assets.gen.dart
@@ -7,119 +7,121 @@
 // ignore_for_file: type=lint
 // ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
 
+import 'package:flutter/widgets.dart';
+
 class $AssetsPngGen {
   const $AssetsPngGen();
 
-  /// File path: assets/png/avatar.png.html
-  String get avatarPng => 'assets/png/avatar.png.html';
+  /// File path: assets/png/avatar.png
+  AssetGenImage get avatar => const AssetGenImage('assets/png/avatar.png');
 
-  /// File path: assets/png/main_logo.png.html
-  String get mainLogoPng => 'assets/png/main_logo.png.html';
+  /// File path: assets/png/main_logo.png
+  AssetGenImage get mainLogo => const AssetGenImage('assets/png/main_logo.png');
 
-  /// File path: assets/png/unnamed.png.html
-  String get unnamedPng => 'assets/png/unnamed.png.html';
+  /// File path: assets/png/unnamed.png
+  AssetGenImage get unnamed => const AssetGenImage('assets/png/unnamed.png');
 
   /// List of all assets
-  List<String> get values => [avatarPng, mainLogoPng, unnamedPng];
+  List<AssetGenImage> get values => [avatar, mainLogo, unnamed];
 }
 
 class $AssetsSvgGen {
   const $AssetsSvgGen();
 
-  /// File path: assets/svg/avatar.svg.html
-  String get avatarSvg => 'assets/svg/avatar.svg.html';
+  /// File path: assets/svg/avatar.svg
+  String get avatar => 'assets/svg/avatar.svg';
 
-  /// File path: assets/svg/back.svg.html
-  String get backSvg => 'assets/svg/back.svg.html';
+  /// File path: assets/svg/back.svg
+  String get back => 'assets/svg/back.svg';
 
-  /// File path: assets/svg/cancelled.svg.html
-  String get cancelledSvg => 'assets/svg/cancelled.svg.html';
+  /// File path: assets/svg/cancelled.svg
+  String get cancelled => 'assets/svg/cancelled.svg';
 
-  /// File path: assets/svg/cart.svg.html
-  String get cartSvg => 'assets/svg/cart.svg.html';
+  /// File path: assets/svg/cart.svg
+  String get cart => 'assets/svg/cart.svg';
 
-  /// File path: assets/svg/clasic.svg.html
-  String get clasicSvg => 'assets/svg/clasic.svg.html';
+  /// File path: assets/svg/clasic.svg
+  String get clasic => 'assets/svg/clasic.svg';
 
-  /// File path: assets/svg/close.svg.html
-  String get closeSvg => 'assets/svg/close.svg.html';
+  /// File path: assets/svg/close.svg
+  String get close => 'assets/svg/close.svg';
 
-  /// File path: assets/svg/delete.svg.html
-  String get deleteSvg => 'assets/svg/delete.svg.html';
+  /// File path: assets/svg/delete.svg
+  String get delete => 'assets/svg/delete.svg';
 
-  /// File path: assets/svg/delivered.svg.html
-  String get deliveredSvg => 'assets/svg/delivered.svg.html';
+  /// File path: assets/svg/delivered.svg
+  String get delivered => 'assets/svg/delivered.svg';
 
-  /// File path: assets/svg/desktop.svg.html
-  String get desktopSvg => 'assets/svg/desktop.svg.html';
+  /// File path: assets/svg/desktop.svg
+  String get desktop => 'assets/svg/desktop.svg';
 
-  /// File path: assets/svg/digital.svg.html
-  String get digitalSvg => 'assets/svg/digital.svg.html';
+  /// File path: assets/svg/digital.svg
+  String get digital => 'assets/svg/digital.svg';
 
-  /// File path: assets/svg/home.svg.html
-  String get homeSvg => 'assets/svg/home.svg.html';
+  /// File path: assets/svg/home.svg
+  String get home => 'assets/svg/home.svg';
 
-  /// File path: assets/svg/in_proccess.svg.html
-  String get inProccessSvg => 'assets/svg/in_proccess.svg.html';
+  /// File path: assets/svg/in_proccess.svg
+  String get inProccess => 'assets/svg/in_proccess.svg';
 
-  /// File path: assets/svg/left_arrow.svg.html
-  String get leftArrowSvg => 'assets/svg/left_arrow.svg.html';
+  /// File path: assets/svg/left_arrow.svg
+  String get leftArrow => 'assets/svg/left_arrow.svg';
 
-  /// File path: assets/svg/location.svg.html
-  String get locationSvg => 'assets/svg/location.svg.html';
+  /// File path: assets/svg/location.svg
+  String get location => 'assets/svg/location.svg';
 
-  /// File path: assets/svg/minus.svg.html
-  String get minusSvg => 'assets/svg/minus.svg.html';
+  /// File path: assets/svg/minus.svg
+  String get minus => 'assets/svg/minus.svg';
 
-  /// File path: assets/svg/phone.svg.html
-  String get phoneSvg => 'assets/svg/phone.svg.html';
+  /// File path: assets/svg/phone.svg
+  String get phone => 'assets/svg/phone.svg';
 
-  /// File path: assets/svg/plus.svg.html
-  String get plusSvg => 'assets/svg/plus.svg.html';
+  /// File path: assets/svg/plus.svg
+  String get plus => 'assets/svg/plus.svg';
 
-  /// File path: assets/svg/postal_code.svg.html
-  String get postalCodeSvg => 'assets/svg/postal_code.svg.html';
+  /// File path: assets/svg/postal_code.svg
+  String get postalCode => 'assets/svg/postal_code.svg';
 
-  /// File path: assets/svg/search.svg.html
-  String get searchSvg => 'assets/svg/search.svg.html';
+  /// File path: assets/svg/search.svg
+  String get search => 'assets/svg/search.svg';
 
-  /// File path: assets/svg/smart.svg.html
-  String get smartSvg => 'assets/svg/smart.svg.html';
+  /// File path: assets/svg/smart.svg
+  String get smart => 'assets/svg/smart.svg';
 
-  /// File path: assets/svg/sort.svg.html
-  String get sortSvg => 'assets/svg/sort.svg.html';
+  /// File path: assets/svg/sort.svg
+  String get sort => 'assets/svg/sort.svg';
 
-  /// File path: assets/svg/user.svg.html
-  String get userSvg => 'assets/svg/user.svg.html';
+  /// File path: assets/svg/user.svg
+  String get user => 'assets/svg/user.svg';
 
-  /// File path: assets/svg/user_menu.svg.html
-  String get userMenuSvg => 'assets/svg/user_menu.svg.html';
+  /// File path: assets/svg/user_menu.svg
+  String get userMenu => 'assets/svg/user_menu.svg';
 
   /// List of all assets
   List<String> get values => [
-        avatarSvg,
-        backSvg,
-        cancelledSvg,
-        cartSvg,
-        clasicSvg,
-        closeSvg,
-        deleteSvg,
-        deliveredSvg,
-        desktopSvg,
-        digitalSvg,
-        homeSvg,
-        inProccessSvg,
-        leftArrowSvg,
-        locationSvg,
-        minusSvg,
-        phoneSvg,
-        plusSvg,
-        postalCodeSvg,
-        searchSvg,
-        smartSvg,
-        sortSvg,
-        userSvg,
-        userMenuSvg
+        avatar,
+        back,
+        cancelled,
+        cart,
+        clasic,
+        close,
+        delete,
+        delivered,
+        desktop,
+        digital,
+        home,
+        inProccess,
+        leftArrow,
+        location,
+        minus,
+        phone,
+        plus,
+        postalCode,
+        search,
+        smart,
+        sort,
+        user,
+        userMenu
       ];
 }
 
@@ -128,4 +130,85 @@ class Assets {
 
   static const $AssetsPngGen png = $AssetsPngGen();
   static const $AssetsSvgGen svg = $AssetsSvgGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(
+    this._assetName, {
+    this.size,
+    this.flavors = const {},
+  });
+
+  final String _assetName;
+
+  final Size? size;
+  final Set<String> flavors;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = false,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.low,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({
+    AssetBundle? bundle,
+    String? package,
+  }) {
+    return AssetImage(
+      _assetName,
+      bundle: bundle,
+      package: package,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
 }
